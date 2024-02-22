0e711fc7dd9f528cc5a293d3f39f1988c3381250
Fredrik Strupe <fredrik@strupe.net>
2024-02-22 16:35:32 +0100

    Initial prototype

---
+++ poe_ctrl.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging as log
+import subprocess
+import sys
+
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto.Random import random
+from urllib3.exceptions import InsecureRequestWarning
+
+
+def get_rsa_key(ip):
+    # Would ideally use the requests lib here but I couldn't get it to NOT
+    # escape curly braces in the url, which curl is able to handle with the
+    # --globoff option.
+    url = f"https://{ip}/csd90d7adf/config/device/wcd?{{EncryptionSetting}}"
+    result = subprocess.run(
+        ["curl", "--globoff", "--insecure", url],
+        check=False,
+        capture_output=True,
+        text=True,
+    )
+
+    # Switch web server is quite janky and doesn't seem to close the connection
+    # properly, leading to a 56 error for curl even though the data was
+    # received successfully.
+    if result.returncode not in [0, 56]:
+        raise Exception("curl failed")
+
+    s = BeautifulSoup(result.stdout, features="xml")
+    pem_key = s.rsaPublicKey.string
+    return RSA.importKey(pem_key)
+
+
+def login(ip, username, password):
+    log.info("Fetching RSA key")
+    rsa_key = get_rsa_key(ip)
+
+    log.info("Encrypting login details")
+    cipher = PKCS1_v1_5.new(rsa_key)
+    plaintext = f"user={username}&password={password}&"
+    ciphertext = cipher.encrypt(plaintext.encode("utf-8"))
+
+    sid = random.randint(10**10, 10**11)
+    cookies = {
+        # 'admin2_numberOfEntriesPerPage': '50',
+        # 'activeLangId': 'English',
+        # 'userStatus': 'ok',
+        "sessionID": f"UserId=127.0.0.1&-{sid}&",
+        # 'usernme': 'admin2',
+        # 'pg': '00000000000000000000000000000000000000000000000000000',
+        # 'LogOff_Reason': 'Manual',
+        # 'firstWelcomeBanner': 'false',
+        # 'isStackableDevice': 'false',
+    }
+
+    params = {
+        "action": "login",
+        "cred": ciphertext.hex(),
+    }
+
+    log.info("Sending login request")
+    url = f"https://{ip}/csd90d7adf/config/System.xml"
+    response = requests.get(
+        url,
+        params=params,
+        cookies=cookies,
+        verify=False,
+    )
+
+    s = BeautifulSoup(response.text, features="xml")
+    if s.statusString.string != "OK":
+        raise Exception("login failed")
+
+    return cookies
+
+
+def get_poe_status(ip, cookies):
+    cookie = f"sessionID={cookies['sessionID']}"
+    url = f"https://{ip}/csd90d7adf/poe/system_poe_interface_m.htm?[pethPsePortTableVT]Filter:(pethPsePortGroupIndex=1)&&(ifOperStatus!=6)&&(rlPethPsePortSupportPoe!=2)"
+    result = subprocess.run(
+        ["curl", "--globoff", "--insecure", url, "-H", f"Cookie: {cookie}"],
+        check=False,
+        capture_output=True,
+        text=True,
+    )
+    s = BeautifulSoup(result.stdout, features="lxml")
+
+    def find_poe_val(s, label, idx):
+        return s.find(
+            "input",
+            {"name": f"{label}$repeat?{idx}"},
+        )["value"]
+
+    port_count = len(
+        s.find_all("input", {"name": lambda x: x and "pethPsePortIndex$repeat" in x})
+    )
+
+    poe_status = []
+    for idx in range(1, port_count + 1):
+        port_status = {
+            "Port Num": str(idx),
+            "PoE Status": (
+                {"1": "Enabled", "2": "Disabled"}[
+                    find_poe_val(s, "pethPsePortAdminEnable", idx)
+                ]
+            ),
+            "Power Consumption": find_poe_val(s, "rlPethPsePortOutputPower", idx),
+            "Operational Status": (
+                {"1": "Disabled", "2": "Searching", "3": "Delivering Power"}[
+                    find_poe_val(s, "pethPsePortDetectionStatus", idx)
+                ]
+            ),
+        }
+        poe_status.append(port_status)
+
+    return poe_status
+
+
+def print_table(poe_status):
+    def print_line(lst):
+        print(str("{:<20}" * len(lst)).format(*lst))
+
+    columns = ("Port Num", "PoE Status", "Power Consumption", "Operational Status")
+    print_line(columns)
+    for status in poe_status:
+        print_line(status.values())
+
+
+def disable_port(ip, cookies, port_idx):
+    set_port_status(ip, cookies, port_idx, False)
+
+
+def enable_port(ip, cookies, port_idx):
+    set_port_status(ip, cookies, port_idx, True)
+
+
+def set_port_status(ip, cookies, port_idx, enabled):
+    query_value = "1" if enabled else "2"
+
+    cookie = f"sessionID={cookies['sessionID']}"
+    url = f"https://{ip}/csd90d7adf/poe/system_poe_interface_e.htm"
+    backend_idx = 48 + port_idx
+    data = f"restoreUrl=[pethPsePortTableVT]Filter:(rlPethPsePortSupportPoe+!=+2)^Query:pethPsePortGroupIndex=1@pethPsePortIndex={backend_idx}&errorCollector=&rlPethPsePortTimeRangeName$VT=Type=100;Access=2;NumOfEnumerations=0;Range0=[0,32];Default+value=&rlPethPsePortSupportPoePlus$VT=Type=0;Access=1;NumOfEnumerations=2;Range0=[1,2];Default+value=1&pethPsePortTableVT$query=OK&pethPsePortGroupIndex$query=1&pethPsePortIndex$query={backend_idx}&pethPsePortAdminEnable$query={query_value}&rlPethPsePortTimeRangeName$query=&rlPethPsePortSupportPoePlus$query=1&pethPsePortTableVT$endQuery=OK"
+    result = subprocess.run(
+        [
+            "curl",
+            "--globoff",
+            "--insecure",
+            url,
+            "-H",
+            f"Cookie: {cookie}",
+            "--data-raw",
+            data,
+        ],
+        check=False,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+
+def logout(ip, cookies):
+    url = f"https://{ip}/csd90d7adf/config/logOff_message.htm"
+    response = requests.get(
+        url,
+        cookies=cookies,
+        verify=False,
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="ProgramName",
+        description="What the program does",
+        epilog="Text at the bottom of help",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
+    args = parser.parse_args()
+
+    if args.verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output.")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+
+    ip = "192.168.1.173"
+    username = "admin2"
+    password = "foobar"
+
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+    try:
+        response = requests.get(
+            f"https://{ip}/csd90d7adf/config/log_off_page.htm",
+            verify=False,
+        )
+    except requests.exceptions.ConnectionError:
+        log.error(f"Switch ({ip}) is unreachable")
+        return 1
+
+    log.info("Logging in...")
+    cookies = login(ip, username, password)
+    log.info("Logged in")
+
+    status = get_poe_status(ip, cookies)
+    print_table(status)
+
+    log.info("Disabling port 3")
+    disable_port(ip, cookies, 3)
+    log.info("Disabled")
+
+    status = get_poe_status(ip, cookies)
+    print_table(status)
+
+    log.info("Enabling port 3")
+    enable_port(ip, cookies, 3)
+    log.info("Enabled")
+
+    status = get_poe_status(ip, cookies)
+    print_table(status)
+
+    log.info("Logging out...")
+    logout(ip, cookies)
+    log.info("Logged out")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
